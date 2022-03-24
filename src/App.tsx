fa026f9e32fa764be9f62613e981af2733f205e2
supercoolx <dm.kondratov.git@gmail.com>
2022-03-24 05:50:41 -0700

    Scroll to top in route

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ScrollToTopWrapper from 'components/util/ScrollToTopWrapper';
 import routes from 'router/routes';
 
 function App() {
 	return (
-		<>
 		<BrowserRouter>
-			<Routes>
-				{
-					routes.map(
-						(prop, key) => <Route path={prop.url} element={prop.component} key={key} />
-					)
-				}
-			</Routes>
+			<ScrollToTopWrapper>
+				<Routes>
+					{
+						routes.map(
+							(prop, key) => <Route path={prop.url} element={prop.component} key={key} />
+						)
+					}
+				</Routes>
+			</ScrollToTopWrapper>
 		</BrowserRouter>
-		</>
 	);
 }
 
---
+++ src/components/util/ScrollToTopWrapper.tsx
@@ -0,0 +1,14 @@
+import { useLayoutEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+const ScrollToTopWrapper = ({children}) => {
+    const location = useLocation();
+	
+    useLayoutEffect(() => {
+      document.documentElement.scrollTo(0, 0);
+    }, [location.pathname]);
+
+    return children
+}
+
+export default ScrollToTopWrapper;
