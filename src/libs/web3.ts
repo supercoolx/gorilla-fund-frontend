592dc8b9a0a2f745df7886f9e6a1eadf26ff79a7
supercoolx <dm.kondratov.git@gmail.com>
2022-05-01 02:05:04 -0700

    Add contract on web3

--- src/libs/web3.ts
+++ src/libs/web3.ts
@@ -1,4 +1,6 @@
 import Web3 from "web3";
+import { AbiItem } from "web3-utils";
+import Abi from "./abi.json";
 
 export const isWeb3Enable = !!window.ethereum;
 
@@ -13,5 +15,6 @@ export const switchNetwork = (chainId: number) => {
 }
 
 const web3 = new Web3(window.ethereum);
+export const contract = new web3.eth.Contract(Abi as AbiItem[], "0x15f8Fc209A1c97a40e64Bf14C8c7D1D9c0541D0f");
 
 export default web3;
