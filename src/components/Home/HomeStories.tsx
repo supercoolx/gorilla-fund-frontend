40fae6450ecc6a6d3582d2f5acae5d96a067effa
Ape Gorilla <97070308+apegorilla@users.noreply.github.com>
2022-05-05 16:53:16 +0700

    Update Homestories
    Updating text

--- src/components/Home/HomeStories.tsx
+++ src/components/Home/HomeStories.tsx
@@ -27,10 +27,10 @@ const HomeStories = () => {
             <div className="flex flex-col px-3 py-24 md:flex-row max-w-[900px] mx-auto">
                 <div className="flex flex-col justify-between pt-0 pr-5 text-sm md:pt-8">
                     <div>
-                        <div className="font-bold text-teal-700">GORILLAFUNDME STORIES</div>
+                        <div className="font-bold text-teal-700">GORILLAFUND STORIES</div>
                         <div className="pt-5 text-2xl font-bold text-black line-clamp-1">{data[index]?.headline}</div>
                         <div className="pt-5 text-gray-500 line-clamp-3">{data[index]?.description}</div>
-                        <div className="py-5 font-bold text-gray-500">Raised $15.0M to help build online school for IT Industry.</div>
+                        <div className="py-5 font-bold text-gray-500">Ape Gorilla NFT project is donating 20% of Public Sales to Gorilla Fund proposals.</div>
                     </div>
                     <div className="flex items-center gap-5">
                         <button className="flex items-center justify-center w-10 h-10 rounded-full border-[1px] border-gray-300 hover:border-teal-700 hover:bg-teal-700 hover:text-white transition-all duration-200" onClick={prevStory}>
