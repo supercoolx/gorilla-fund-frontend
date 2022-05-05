adb45f3b5bd625baa98cdab10324cb2904094cf4
Ape Gorilla <97070308+apegorilla@users.noreply.github.com>
2022-05-05 17:05:59 +0700

    Fix StartFundraising

--- src/components/Footer/StartFundraising.tsx
+++ src/components/Footer/StartFundraising.tsx
@@ -5,10 +5,10 @@ import { URL } from "libs/constants";
 const StartFundraising = () => {
     return (
         <div className="flex flex-col items-center px-3 py-12 bg-teal-700 gap-7">
-            <div className="text-2xl font-bold text-center text-white">Ready to start fundraising?</div>
+            <div className="text-2xl font-bold text-center text-white">Ready to start Fundraising?</div>
             <div className="flex flex-col items-center text-sm text-teal-50">
-                <p className="text-center">"My life has changed forever because of Norma and what happened.</p>
-                <p className="text-center">It gave me hope again. Changing someone's life will change your own forever."</p>
+                <p className="text-center">"Change your life with Gorilla Fund today!</p>
+                <p className="text-center">Receive the support from the community and public and start receiving crypto donations!"</p>
             </div>
             <Link to={URL.FUNDRAISE} className="px-5 py-2 font-bold text-teal-700 border-[1px] rounded-[4px] border-teal-700 bg-white hover:bg-teal-700 hover:border-white hover:text-white transition-all duration-200">Start a GorillaFund</Link>
         </div>
